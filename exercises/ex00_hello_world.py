40afd7123f89abddf4938bfe9b1fa210d6920b18
Your Name <amoghrao@unc.edu>
2024-08-22 16:52:12 -0400

    Finished Exercise 00

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first excersize in COMP110!"""
+
+__author__ = "730672928"
+
+
+def greet(name: str) -> str:
+    """A welcoming first funtcion definition."""
+    return "Hello, " + name + "!"
