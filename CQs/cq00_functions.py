739d2d8bad0d815b2825d8e914ccafbb25dd076c
Your Name <amoghrao@unc.edu>
2024-08-26 16:06:09 -0400

    Finished CQ00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,12 @@
+"""Functions Challenge Question"""
+
+__author__ = "730672928"
+
+
+def mimic(message: str) -> str:
+    """This will return back an inputed message (ie. mimic!)"""
+    return message
+
+
+if __name__ == "__main__":
+    print(mimic(message=input("What is your message?")))
---
+++ lessons/functions.py
@@ -0,0 +1,15 @@
+"""Practice with functions"""
+
+# from random import randint
+
+# print(randint(3, 17))
+
+
+# Function Definition
+def sum(num1: int, num2: int) -> int:  # signature of the function
+    """Add two numbers together."""
+    return num1 + num2
+
+
+# Function Call
+print(sum(num1=2, num2=13))  # <- arguments
